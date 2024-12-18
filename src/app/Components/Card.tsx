d01a3488cf9a9645772e10cc862a04f63b3200f6
Leopold <leopold.mtr@outlook.com>
2024-12-18 18:12:27 +0900

    added conditionnality for pictures size on mobile view

--- src/app/Components/Card.tsx
+++ src/app/Components/Card.tsx
@@ -1,5 +1,8 @@
+'use client';
+
 import Image from "next/image";
 import Link from "next/link";
+import { useState, useEffect } from "react";
 
 interface CardProps {
     id?: number;
@@ -10,40 +13,72 @@ interface CardProps {
     mode?: string;
 }
 
-const Card: React.FC<CardProps> = ({id, title, image, year, mode}) => {
-    const width = mode === 'portrait' ? 355 : 800;
+const Card: React.FC<CardProps> = ({ id, title, image, year, mode }) => {
+    const [dimensions, setDimensions] = useState({ 
+        width: 355, 
+        height: 236,
+        isMobile: false,
+        heightOfSection: 600,
+    });
+
+    useEffect(() => {
+        const calculateDimensions = () => {
+            const width = mode === 'portrait' ? 355 : 800;
+            const heightOfPicture = 600;
+            const isMobile = window.innerWidth < 768;
+            setDimensions({
+                width: isMobile ? 355 : width,
+                height: isMobile && mode === 'portrait' ? 236 : heightOfPicture,
+                heightOfSection: isMobile && mode === 'landscape' ? 300 : 600,
+                isMobile,
+            });
+        };
+
+        // Initial calculation
+        calculateDimensions();
+
+        // Add event listener for window resize
+        window.addEventListener('resize', calculateDimensions);
+
+        // Cleanup event listener on unmount
+        return () => {
+            window.removeEventListener('resize', calculateDimensions);
+        };
+    }, [mode]);
 
     return (
-        <section 
-        className=""
-        style={{
-            height: 600,
-            width:width}}>
-        <Link href={`${title}`}>
-            <div className="mb-4">
-                <Image
-                    height={500}
-                    width={width}
-                    src={image || ''}
-                    alt={title || 'Image'}
-                    className="rounded-md"
-                    loading="lazy"
-                />
-            </div>
-            <div className="flex flex-col">
-                <div className="flex flex-row justify-between  w-full">
-                    <h3 className="text-secondary font-light text-base tracking-widest opacity-80">
-                        / {id}
-                    </h3>
-                    <p className="text-typo tracking-widest font-semibold text-base">
-                        {title}
-                    </p>
+        <section
+            className=""
+            style={{
+                height: dimensions.heightOfSection,
+                width: dimensions.width,
+            }}
+        >
+            <Link href={`${title || ''}`}>
+                <div className="mb-4">
+                    <Image
+                        height={dimensions.height}
+                        width={dimensions.width}
+                        src={image || ''}
+                        alt={title || 'Image'}
+                        className="rounded-md"
+                        loading="lazy"
+                    />
                 </div>
-                <div className="flex justify-end w-full text-secondary font-light text-sm tracking-widest opacity-80">
-                    <p>{year}</p>
+                <div className="flex flex-col">
+                    <div className="flex flex-row justify-between w-full">
+                        <h3 className="text-secondary font-light text-base tracking-widest opacity-80">
+                            / {id}
+                        </h3>
+                        <p className="text-typo tracking-widest font-semibold text-base">
+                            {title}
+                        </p>
+                    </div>
+                    <div className="flex justify-end w-full text-secondary font-light text-sm tracking-widest opacity-80">
+                        <p>{year}</p>
+                    </div>
                 </div>
-            </div>
-         </Link>
+            </Link>
         </section>
     );
 };
--- src/app/Components/CardGallery.tsx
+++ src/app/Components/CardGallery.tsx
@@ -11,17 +11,15 @@ interface CardGalleryProps {
 }
 
 const CardGallery: React.FC<CardGalleryProps> = ({ id, modeP, title, src, alt }) => {
-    const defaultWidth = modeP === 'portrait' ? 355 : 800; // Largeur par défaut pour md et supérieur
-
-    // Détermine la largeur en fonction de l'écran
+    const defaultWidth = modeP === 'portrait' ? 355 : 800; 
     const imageWidth = typeof window !== "undefined" && window.innerWidth < 768 ? 355 : defaultWidth;
 
     return (
-        <section className="flex flex-row rounded-md" style={{ width: imageWidth }}>
+        <section className="flex flex-row rounded-md" style={{ width: imageWidth}}>
             <Image
                 key={id}
                 height={500}
-                width={imageWidth} // Largeur dynamique ici
+                width={imageWidth} //dynamic width
                 src={src}
                 alt={title || 'Image'}
                 className="rounded-md object-cover"
