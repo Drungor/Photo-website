ac5cd8dc1b20391befeedcd109741314e7db8511
Leopold <leopold.mtr@outlook.com>
2024-12-22 18:43:25 +0900

    refactor layout component for improved readability and structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,13 @@
-import type { Metadata } from "next";
-import { Noto_Sans_JP } from 'next/font/google'
+import { Noto_Sans_JP } from 'next/font/google';
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
-import clsx from 'clsx'
+import clsx from 'clsx';
 import "./globals.css";
 
 const notoSansJP = Noto_Sans_JP({
   subsets: ['latin'],
   variable: '--font-noto-sans-jp',
-})
-
-
-export const metadata: Metadata = {
-  title: "Leo's Photo",
-  description: "Follow me on my journey to make greater pictures.",
-};
+});
 
 export default function RootLayout({
   children,
@@ -22,19 +15,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='ja' className={clsx(notoSansJP.variable, 'font-sans')}>
+    <html lang="ja" className={clsx(notoSansJP.variable, 'font-sans')}>
       <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="description" content={metadata.description} />
+        <meta name="description" content="Follow me on my journey to make greater pictures." />
+        <title>Leo's Photo</title>
         <style>
-          writing-mode: horizontal-tb; 
+          writing-mode: horizontal-tb;
         </style>
       </head>
-      <body className="  md:mx-20 relative">
-      <Header />
+      <body className="md:mx-20 relative">
+        <Header />
         {children}
-      <Footer />
+        <Footer />
       </body>
     </html>
   );
